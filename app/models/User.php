32de0e5d1352637ab7ae7ce830de6b1a7ccd516b
Maya Gort <mgort@mymail.mines.edu>
2014-11-16 16:57:01 -0700

    trying to fix things

--- app/models/User.php
+++ app/models/User.php
@@ -1,17 +1,26 @@
 <?php
+
+use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\Reminders\RemindableTrait;
+use Illuminate\Auth\Reminders\RemindableInterface;
 
-class User extends Eloquent implements UserInterface {
+class User extends Eloquent implements UserInterface, RemindableInterface {
 
-	protected $fillable = array('lastName', 'firstName', 'CWID', 'email', 'password', 'is_admin');
+	use UserTrait, RemindableTrait;
 
-	public function getAuthPassword() {
-		return $this->password;
-	}
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'users';
 
-	public function getAuthIdentifier() {
-		return $this->getKey();
-	}
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	protected $hidden = array('password', 'remember_token');
+
 }
-
-?>
