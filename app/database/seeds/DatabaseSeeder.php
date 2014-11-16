99ab4a2fa9fd1d83e7cbac6da8cc217853785a0c
Maya Gort <mgort@mymail.mines.edu>
2014-11-16 16:41:53 -0700

    made models and migration file. It doesn't work yet

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -10,6 +10,7 @@ class DatabaseSeeder extends Seeder {
 	public function run()
 	{
 		Eloquent::unguard();
+		$this->call('');
 
 		// $this->call('UserTableSeeder');
 	}
--- app/models/User.php
+++ app/models/User.php
@@ -1,26 +1,17 @@
 <?php
-
-use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
-use Illuminate\Auth\Reminders\RemindableTrait;
-use Illuminate\Auth\Reminders\RemindableInterface;
 
-class User extends Eloquent implements UserInterface, RemindableInterface {
+class User extends Eloquent implements UserInterface {
 
-	use UserTrait, RemindableTrait;
+	protected $fillable = array('lastName', 'firstName', 'CWID', 'email', 'password', 'is_admin');
 
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'users';
+	public function getAuthPassword() {
+		return $this->password;
+	}
 
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
-	protected $hidden = array('password', 'remember_token');
-
+	public function getAuthIdentifier() {
+		return $this->getKey();
+	}
 }
+
+?>
