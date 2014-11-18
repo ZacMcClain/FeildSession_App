1d99175af3aec16e0f3289d959a71572489de3be
Maya Gort <mgort@mymail.mines.edu>
2014-11-18 10:51:53 -0700

    added the new migration file

---
+++ app/database/migrations/2014_11_18_174118_add_users_and_projects_tables.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUsersAndProjectsTables extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::dropIfExists('users');
+		Schema::create('users', function($table) {
+			$table->increments('id');
+			$table->string('lastName');
+			$table->string('firstName');
+			$table->string('CWID');
+			$table->string('email');
+			$table->boolean('is_admin');
+		});
+
+		Schema::dropIfExists('projects');
+		Schema::create('projects', function($table){
+			$table->increments('id');
+			$table->string('company');
+			$table->string('title');
+			$table->integer('min');
+			$table->integer('max');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+		Schema::drop('projects');
+	}
+
+}
